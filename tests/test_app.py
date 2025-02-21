a21522ff77af49aa97886ea02edf0888bd183897
izabella-m <izabella.pessoa03@outlook.com>
2025-02-21 02:21:21 -0300

    Tests of routes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,65 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_test_read_root_deve_retornar_ok_e_olamundo():
-    client = TestClient(app)  # Arrange
-
-    response = client.get('/')  # Act
+def test_test_read_root_deve_retornar_ok_e_olamundo(client):
+    response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá mundooo'}
+    assert response.json() == {'message': 'Olá'}
+
+
+def test_create_users(client):
+    response = client.post(
+        '/users/',
+        json={
+            'name': 'teste',
+            'email': 'teste@teste.com',
+            'age': 12,
+            'password': 'teste'
+        }
+    )
+
+    assert response.status_code == HTTPStatus.CREATED 
+    assert response.json() == {
+        'name': 'teste',
+        'email': 'teste@teste.com',
+        'age': 12,
+        'id': 1
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'users': [
+            {
+                'name': 'teste',
+                'email': 'teste@teste.com',
+                'age': 12,
+                'id': 1
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1', 
+        json={
+            'name': 'teste',
+            'email': 'teste@teste.com',
+            'age': 12,
+            'password': 'teste', 
+            'id': 1
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+            'name': 'teste',
+            'email': 'teste@teste.com',
+            'age': 12,
+            'id': 1  
+    }
