960c27bd1ec4ff72d2f1892f0e44de4329f7e959
izabella-m <izabella.pessoa03@outlook.com>
2025-04-06 23:12:49 -0300

    feat: implementa model User usando mapped dataclass do SQLAlchemy

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -1,13 +1,20 @@
-from sqlalchemy.orm import registry, Mapped, mapped_column
+from datetime import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column, registry
 
 table_registry = registry()
-@table_registry.mapped_as_dataclass # Data class que representa uma tabela de banco
 
-class User: 
-    __tablename__ = 'users' 
-    
-    id: Mapped[int] = mapped_column(init=False,primary_key=True) 
+
+@table_registry.mapped_as_dataclass  # Data class que representa uma tabela de banco
+class User:
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
     email: Mapped[str] = mapped_column(unique=True)
-    created_at: Mapped[datetime] 
+    created_at: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
