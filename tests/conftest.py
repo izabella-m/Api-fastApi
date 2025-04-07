3024a27c847abe0d8471cc0b060cac107f6794b0
izabella-m <izabella.pessoa03@outlook.com>
2025-04-06 23:10:34 -0300

    feat(tests): adiciona fixture de sessão SQLite em memória para testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,21 @@
 import pytest
+from sqlalchemy import create_engine
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
+from fast_zero.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture
+def session ():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine) # 
+    
+    with Session(engine) as session:
+        yield session
+    
+    table_registry.metadata.drop_all(engine)
