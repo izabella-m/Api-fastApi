028df0ba54a9d4469e80fe53d221647493c8123c
izabella-m <izabella.pessoa03@outlook.com>
2025-05-02 23:35:00 -0300

    test: create database test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,30 @@
 import pytest
-from sqlalchemy import create_engine
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
 
 from fast_zero.app import app
+from fast_zero.database import get_session
 from fast_zero.models import table_registry
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
+
 
 @pytest.fixture
-def session ():
+def session():
     engine = create_engine('sqlite:///:memory:')
-    table_registry.metadata.create_all(engine) # 
-    
+    table_registry.metadata.create_all(engine)
+
     with Session(engine) as session:
         yield session
-    
+
     table_registry.metadata.drop_all(engine)
--- tests/test_app.py
+++ tests/test_app.py
@@ -3,39 +3,35 @@ from http import HTTPStatus
 
 def test_create_users(client):
     response = client.post(
-        '/users/',
+        "/users/",
         json={
-            'name': 'teste',
-            'email': 'teste@teste.com',
-            'age': 12,
-            'password': 'teste'
-        }
+            "name": "teste",
+            "email": "teste@teste.com",
+            "age": 12,
+            "password": "teste",
+        },
     )
 
-    assert response.status_code == HTTPStatus.CREATED 
+    assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        'name': 'teste',
-        'email': 'teste@teste.com',
-        'age': 12,
-        'id': 1
+        "name": "teste",
+        "email": "teste@teste.com",
+        "age": 12,
+        "id": 1,
     }
 
 
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get("/users/")
 
     assert response.status_code == HTTPStatus.OK
 
     assert response.json() == {
-        'users': [
-            {
-                'name': 'teste',
-                'email': 'teste@teste.com',
-                'age': 12,
-                'id': 1
-            }
+        "users": [
+            {"name": "teste", "email": "teste@teste.com", "age": 12, "id": 1}
         ]
     }
+
 
 # def test_read_users_for_id(client):
 #         response = client.get('/users/1', json={
@@ -54,25 +50,27 @@ def test_read_users(client):
 #         # 'age': 12
 #     }
 
+
 def test_update_user(client):
     response = client.put(
-        '/users/1', 
+        "/users/1",
         json={
-            'name': 'teste',
-            'email': 'teste@teste.com',
-            'age': 12,
-            'password': 'teste', 
-            'id': 1
+            "name": "teste",
+            "email": "teste@teste.com",
+            "age": 12,
+            "password": "teste",
+            "id": 1,
         },
     )
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
-            'name': 'teste',
-            'email': 'teste@teste.com',
-            'age': 12,
-            'id': 1  
+        "name": "teste",
+        "email": "teste@teste.com",
+        "age": 12,
+        "id": 1,
     }
 
+
 def test_delete_user(client):
-    response = client.delete('/users/1')
-    assert response.json() ==  {'message': 'user deleted'}
+    response = client.delete("/users/1")
+    assert response.json() == {"message": "user deleted"}
